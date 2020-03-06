7f4c574bb797509f62ac42d6fbcd1de7162bee82
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-03-06 05:27:42 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,15 @@
-class Post 
-  attr_accessor :name 
-  
-  @@all = []
-  
-  def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
-  
-   def self.all
-    @@all 
-  end 
-  
-  
-end 
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+end
