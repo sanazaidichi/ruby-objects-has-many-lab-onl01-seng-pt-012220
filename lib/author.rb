396ed4ac0e97a37aaf7ba495d47b608217bc0555
sanazaidichi <sana.rizvi.5@gmail.com>
2020-06-24 23:57:12 -0500

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -20,7 +20,7 @@ class Author
     def add_post(post)
         post.author = self
     end
-  
+
     def add_post_by_title(post_title)
       new_post = Post.new(post_title)
       add_post(new_post)
